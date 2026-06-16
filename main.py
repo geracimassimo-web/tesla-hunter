2f5ae722918de628027d357a3126e259b218247a
geracimassimo-web <geraci.massimo@gmail.com>
2026-06-16 23:41:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,24 @@ CHAT_ID = "2022439793"
 
 def send(msg):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    r = requests.post(url, data={
+    requests.post(url, data={
         "chat_id": CHAT_ID,
         "text": msg
     })
-    print(r.text)
 
 def main():
-    send("TEST DIRETTO")
+    links = [
+        "https://www.autoscout24.it/lst/tesla/model-y?sort=price&desc=0&priceto=31500",
+        "https://www.automobile.it/auto-usate/tesla/model-y/?prezzo_max=31500",
+        "https://www.subito.it/annunci-italia/vendita/auto/?q=tesla%20model%20y"
+    ]
+
+    msg = "🚗 TESLA ALERT\n\nControlla qui le nuove occasioni:\n\n"
+
+    for link in links:
+        msg += link + "\n\n"
+
+    send(msg)
 
 if __name__ == "__main__":
     main()
