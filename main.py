2c79c0460933c0dfc6c0526efa9ce92fd56c85ea
geracimassimo-web <geraci.massimo@gmail.com>
2026-06-16 17:48:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 
-TELEGRAM_TOKEN = 8573311691:AAE5g32_JSYEHk-eiGiTs1OoupQfeUK0-Uc
-CHAT_ID = 2022439793
+TELEGRAM_TOKEN = "8573311691:AAE5g32_JSYEHk-eiGiTs1OoupQfeUK0-Uc"
+CHAT_ID = "2022439793"
 
 def send(msg):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
