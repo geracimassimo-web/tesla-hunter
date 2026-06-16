ff8c1f1b041482b4859a76f11b3a9f7862186246
geracimassimo-web <geraci.massimo@gmail.com>
2026-06-16 23:39:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 import requests
-from playwright.sync_api import sync_playwright
 
 TELEGRAM_TOKEN = "8573311691:AAE5g32_JSYEHk-eiGiTs1OoupQfeUK0-Uc"
-CHAT_ID = "022439793"
+CHAT_ID = "2022439793"
 
 def send(msg):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    requests.post(url, data={
+    r = requests.post(url, data={
         "chat_id": CHAT_ID,
         "text": msg
     })
+    print(r.text)
 
 def main():
-    link = "https://www.automobile.it/auto-usate/tesla/model-y/?prezzo_max=31500"
-
-    msg = f"🚗 Controlla qui le Tesla disponibili:\n\n{link}"
-
-    send(msg)
+    send("TEST DIRETTO")
 
 if __name__ == "__main__":
     main()
