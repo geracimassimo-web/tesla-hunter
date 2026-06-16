e87a7cf2c0928a55680629898cb0154256667b74
geracimassimo-web <geraci.massimo@gmail.com>
2026-06-16 23:34:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,45 +11,12 @@ def send(msg):
         "text": msg
     })
 
-def get_links():
-    links = []
-
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        page = browser.new_page()
-
-        page.goto("https://www.autoscout24.it/lst/tesla/model-y?sort=price&desc=0&priceto=31500")
-
-        # aspetta che gli annunci siano caricati
-        page.wait_for_selector("article")
-
-        elements = page.query_selector_all("article a")
-
-        for el in elements:
-            href = el.get_attribute("href")
-            if href and "/annunci/" in href:
-                full_link = "https://www.autoscout24.it" + href
-                links.append(full_link)
-
-        browser.close()
-
-    return list(set(links))
-
-
 def main():
-    links = get_links()
+    link = "https://www.automobile.it/auto-usate/tesla/model-y/?prezzo_max=31500"
 
-    if not links:
-        send("❌ Nessuna Tesla trovata")
-        return
-
-    msg = "🚗 TESLA TROVATE:\n\n"
-
-    for link in links[:5]:
-        msg += link + "\n\n"
+    msg = f"🚗 Controlla qui le Tesla disponibili:\n\n{link}"
 
     send(msg)
-
 
 if __name__ == "__main__":
     main()
